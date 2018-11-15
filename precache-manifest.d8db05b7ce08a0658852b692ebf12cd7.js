095c2f16d50b90a95450e666e5e2865430a7b73a
marc <mfmunic@gmail.com>
2018-11-15 12:50:23 -0800

    Updates

---
+++ precache-manifest.d8db05b7ce08a0658852b692ebf12cd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/homepage/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "c9f2de54d44bb9049b00",
+    "url": "/homepage/static/js/runtime~main.c9f2de54.js"
+  },
+  {
+    "revision": "21935848236af45e50c5",
+    "url": "/homepage/static/js/main.21935848.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/homepage/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "21935848236af45e50c5",
+    "url": "/homepage/static/css/main.e4cf5141.chunk.css"
+  },
+  {
+    "revision": "73b843be0cad31892b2bccfd90d62b76",
+    "url": "/homepage/index.html"
+  }
+];
