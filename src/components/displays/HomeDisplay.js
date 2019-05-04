2136e81de287f04b2a163201af4f072b95c32645
Marc <mfmunic@gmail.com>
2019-05-04 15:41:31 -0700

    sass added

--- src/components/displays/HomeDisplay.js
+++ src/components/displays/HomeDisplay.js
@@ -1,49 +1,47 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 // import homeText from "./texts/HomeText";
 
 class HomeDisplay extends Component {
   render() {
     return (
-      <div id="homeDisplay">
+      <div className="homeDisplay">
         {/* <h1 className="blue-Text">Hello, World!</h1>
         <p>{homeText}</p> */}
-        <div id="homeImages">
+        <div className="homeImages">
           <img
-            src={require("../utils/images/Marc.png")}
+            src={require('../utils/images/Marc.png')}
             id="linkedInImg"
             className="homeImg"
             alt="Marc"
           />
           <img
-            src={require("../utils/images/marcandhog.JPG")}
+            src={require('../utils/images/marcandhog.JPG')}
             id="mahImg"
             className="homeImg"
             alt="Marc"
           />
           <img
-            src={require("../utils/images/marcandtractor.png")}
+            src={require('../utils/images/marcandtractor.png')}
             id="matImg"
             className="homeImg"
             alt="Marc"
           />
           <img
-            src={require("../utils/images/marcandstatue.jpg")}
+            src={require('../utils/images/marcandstatue.jpg')}
             id="masImg"
             className="homeImg"
             alt="Marc"
           />
         </div>
-        <div id="homeDisplay-intro">
-          <h1 id="homeDisplay-intro-Line1">
-            Hello, I'm{" "}
-            <span id="homeDisplay-intro-Line1--myName" className="blue-Text">
-              Marc F. Munic
-            </span>
-            .
+        <div className="homeDisplay-intro">
+          <h1 className="homeDisplay-intro-line z5">
+            Hello, I'm <span className="Text--blue">Marc F. Munic</span>.
           </h1>
-          <h1 id="homeDisplay-intro-Line2">I'm a full-stack web developer.</h1>
+          <h1 className="homeDisplay-intro-line">
+            I'm a full-stack web developer.
+          </h1>
         </div>
       </div>
     );
